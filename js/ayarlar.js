b8303f4669aa4a1116f3473e4fe572ce5789192a
mustafataskin1986 <mustafataskin1986@gmail.com>
2026-09-09 11:19:09 +0300

    Update ayarlar.js

--- js/ayarlar.js
+++ js/ayarlar.js
@@ -1,1 +1,5 @@
-fhvb
+logoutBtn.addEventListener('click', () => {
+    localStorage.removeItem('aurachat_user');
+    localStorage.removeItem('aurachat_contacts_cache');
+    location.reload();
+});
