19c73aff655c30e98edeef4722d2e1c9b03e2eb4
mustafataskin1986 <mustafataskin1986@gmail.com>
2026-09-13 22:06:31 +0300

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import {
+    initializeFirestore,
+    persistentLocalCache,
+    persistentSingleTabManager
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
@@ -12,7 +16,16 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+
+// Kalıcı yerel önbellek: bir sohbeti bir kez açtıktan sonra, tekrar
+// girişlerde Firestore önce yerel diskten (IndexedDB) anında gösterir,
+// arka planda sessizce sunucuyla senkronize eder - network beklemesi olmaz.
+export const db = initializeFirestore(app, {
+    localCache: persistentLocalCache({
+        tabManager: persistentSingleTabManager({})
+    })
+});
+
 export const auth = getAuth(app);
 
 auth.languageCode = 'tr';
